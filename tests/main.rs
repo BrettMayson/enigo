d4996dbba7a900583ba0272016908031f20ab223
BrettMayson <brett@mayson.io>
2023-01-26 18:00:07 -0600

    install firefox

--- tests/main.rs
+++ tests/main.rs
@@ -1,7 +1,7 @@
-// #[cfg(feature = "test_browser")]
+#[cfg(feature = "test_browser")]
 mod browser;
 
-// #[cfg(feature = "test_browser")]
+#[cfg(feature = "test_browser")]
 #[test]
 fn browser_events() {
     let recv = browser::launch();
